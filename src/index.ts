d12c661d80230f79c52650a86055b52ecedbf27c
WALID SADAOUI <walidsadaoui@hotmail.fr>
2021-08-06 11:46:23 +0200

    chore(husky): setup

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,18 @@ import cors from 'cors';
 import helmet from 'helmet';
 import errorHandler from './middlewares/error.middleware';
 import notFoundHandler from './middlewares/not-found.middleware';
+import config from './config';
 
 const app = express();
 app.use(helmet());
-app.use(logger('dev'));
-app.use(cors());
+config.nodeEnv == 'development' && app.use(logger('dev'));
+app.use(
+  cors({
+    credentials: true,
+    origin: config.clientUrl,
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+  })
+);
 app.use(express.json());
 
 app.use(errorHandler);
